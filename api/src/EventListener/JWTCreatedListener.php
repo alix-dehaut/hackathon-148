8152e5325221643abbc89bd3e604ace57b780456
Lucas <xz351@hotmail.fr>
2020-03-04 15:50:18 +0100

    Customize JWT token

---
+++ api/src/EventListener/JWTCreatedListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+
+class JWTCreatedListener
+{
+    /**
+     * Replaces the data in the generated
+     *
+     * @param JWTCreatedEvent $event
+     *
+     * @return void
+     */
+    public function onJWTCreated(JWTCreatedEvent $event)
+    {
+        /** @var $user \AppBundle\Entity\User */
+        $user = $event->getUser();
+
+        // add new data
+        $payload['userId'] = $user->getId();
+        $payload['username'] = $user->getUsername();
+
+        $event->setData($payload);
+    }
+}
